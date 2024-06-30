f0de2c3cda07729e31521636bf870b1216ae7b7c
arthur <artyr007@gmail.com>
2024-07-01 02:53:09 +0500

    delete: capitalize.test.js due to unifying all tests in single file

--- capitalize.test.js
+++
@@ -1,9 +0,0 @@
-import capitalize from "./capitalize.js";
-
-test("Takes a string and returns it with first character capitalized", () => {
-  expect(capitalize("london")).toBe("London");
-});
-
-test("Takes a string and returns it with first character capitalized(2)", () => {
-  expect(capitalize("canada")).toBe("Canada");
-});
