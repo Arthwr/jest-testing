c448ba6f0a64d0a16034a3ee6d494b9ae11ea63c
arthur <artyr007@gmail.com>
2024-07-01 02:54:21 +0500

    test: add function and unit test function for reverseString

--- unitTest.test.js
+++ unitTest.test.js
@@ -1,4 +1,5 @@
 import capitalize from "./capitalize.js";
+import reverseString from "./reverseString.js";
 
 test("Takes a string and returns it with first character capitalized", () => {
   expect(capitalize("london")).toBe("London");
@@ -6,4 +7,17 @@ test("Takes a string and returns it with first character capitalized", () => {
 
 test("Takes a string and returns it with first character capitalized(2)", () => {
   expect(capitalize("canada")).toBe("Canada");
+});
+
+describe("String reversal function", () => {
+  test.each([
+    ["12345", "54321"],
+    [
+      "The path to success is a discipline",
+      "enilpicsid a si sseccus ot htap ehT",
+    ],
+    ["Hello, World!", "!dlroW ,olleH"],
+  ])('Reverses "%s" to "%s"', (input, expected) => {
+    expect(reverseString(input)).toBe(expected);
+  });
 });
