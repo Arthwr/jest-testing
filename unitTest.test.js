8ec0990d706780c849311246f51960f5ecd5d69c
arthur <artyr007@gmail.com>
2024-07-01 20:00:52 +0500

    test: add calculator object with add, subtract, divide, multiply methods and test cases for these methods

--- unitTest.test.js
+++ unitTest.test.js
@@ -1,5 +1,6 @@
 import capitalize from "./capitalize.js";
 import reverseString from "./reverseString.js";
+import calculator from "./calculator.js";
 
 describe("String with first character capitalized function", () => {
   test.each([
@@ -21,5 +22,58 @@ describe("String reversal function", () => {
     ["Hello, World!", "!dlroW ,olleH"],
   ])('Reverses "%s" to "%s"', (input, expected) => {
     expect(reverseString(input)).toBe(expected);
+  });
+});
+
+describe("Calculator functions:", () => {
+  describe("add", () => {
+    test.each([
+      [2, 2, 4],
+      [-2, -2, -4],
+      [55, -44, 11],
+    ])("Adds (%i, %i) to be %d", (a, b, expected) => {
+      const result = calculator().add(a, b);
+      expect(result).toBe(expected);
+    });
+  });
+
+  describe("subtract", () => {
+    test.each([
+      [2, 2, 0],
+      [-5, -8, 3],
+      [9245, -84244, 93489],
+    ])("Subtracts (%i, %i) to be %d", (a, b, expected) => {
+      const result = calculator().subtract(a, b);
+      expect(result).toBe(expected);
+    });
+  });
+
+  describe("divide", () => {
+    test.each([
+      [2, 2, 1],
+      [4, 0, Infinity],
+      [-1, 0, -Infinity],
+      [14, 2, 7],
+      [0, 0, NaN],
+    ])("Divides %i by %i to get %d", (a, b, expected) => {
+      const result = calculator().divide(a, b);
+
+      if (isNaN(expected)) {
+        expect(result).toBeNaN();
+      } else {
+        expect(result).toBe(expected);
+      }
+    });
+  });
+
+  describe("multiply", () => {
+    test.each([
+      [2, 0, 0],
+      [2, 2, 4],
+      [-2, 10, -20],
+    ])("Multiply %i by %i to get %d", (a, b, expected) => {
+      const result = calculator().multiply(a, b);
+      expect(result).toBe(expected);
+    });
   });
 });
